0c0e2e031c71e2ccc4a642fefd8a9fe2125a624b
cwpeng-cn <cw.peng@foxmail.com>
2023-05-24 21:12:07 +0800

    update: change BCEWithLogitsLoss with BCELoss

--- loss/weight_loss.py
+++ loss/weight_loss.py
@@ -1,17 +1,25 @@
 import torch
 from torch import nn
-from torch.nn import BCEWithLogitsLoss
+from torch.nn import BCELoss
 
 
 class WeightLoss(nn.Module):
     def __init__(self):
         super(WeightLoss, self).__init__()
-        self.class_criterion = BCEWithLogitsLoss()
+        self.class_criterion = BCELoss()
 
     def forward(self, landmark_gt, landmarks, visables, pre_visable,
                 actually_visable):
-        # landmark_gt => N,64,2
-        # visables => N,64
+        """
+            l2 loss + bce loss
+        Args
+            @landmark_gt: gt landmark
+            @landmarks: predict landmark
+            @visable: landmark gt visibility (original image)
+            @pre_visable: predict landmark visibility
+            @actually_visable: landmark gt visibility (croped,scaled image)
+        """
+
         l2_distant = torch.sum(
             (landmark_gt - landmarks) * (landmark_gt - landmarks), axis=-1)
         l2_distant = l2_distant * visables
@@ -24,5 +32,5 @@ if __name__ == "__main__":
     landmark_gt = torch.rand(2, 64, 2)
     landmarks = torch.rand(2, 64, 2)
     visables = torch.ones(2, 64)
-    value = loss(landmark_gt, landmarks, visables)
+    value = loss(landmark_gt, landmarks, visables, visables * 1.0, visables)
     print(value)
